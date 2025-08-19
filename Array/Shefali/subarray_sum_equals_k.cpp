908bc3146e19704e947091109b6b154b798d3fa2
Shefali Kaushal <120242919+Shefalikaushal13@users.noreply.github.com>
2025-08-19 23:39:40 +0530

    Day 8 solved 4 medium problems

---
+++ Array/Shefali/subarray_sum_equals_k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int total = 0, count = 0;
+
+        for (int n : nums) {
+            total += n;
+
+            if (mp.find(total - k) != mp.end()) {
+                count += mp[total - k];
+            }
+
+            mp[total]++;
+        }
+
+        return count;
+    }
+};
