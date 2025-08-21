1b68b8cf9d2dcc4769a2d57804ec8765592f0037
Shefali Kaushal <120242919+Shefalikaushal13@users.noreply.github.com>
2025-08-21 11:25:24 +0530

    Day 9 solved 2 medium problems

--- Binary Search/Shefali/search_in_rotated_sorted_arr.cpp
+++ Binary Search/Shefali/search_in_rotated_sorted_arr.cpp
@@ -3,11 +3,14 @@ public:
     int search(vector<int>& nums, int target) {
         int low=0;
         int high=nums.size()-1;
+
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
+
+            //left half is sorted
             if(nums[low]<=nums[mid]){
                 if(nums[low]<=target && target<nums[mid]){
                     high=mid-1;
@@ -16,9 +19,14 @@ public:
                     low=mid+1;
                 }
             }
+
+            //right half is sorted
             else{
                 if(nums[mid]<target && target<=nums[high]){
                     low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
